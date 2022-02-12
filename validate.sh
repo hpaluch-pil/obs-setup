968a0108a2bd264e147e3d115c085a266d66946a
Henryk Paluch <henryk.paluch@pickering.cz>
2022-02-12 12:12:31 +0100

    Reduced CentOS repos to bare minimum

---
+++ validate.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Validate XML files where possible
+
+set -euo pipefail
+#set -x
+cd $(dirname $0)
+for i in *-project.xml
+do
+	echo "Validating project '$i':"
+	echo -n "- "
+	xmllint --noout --relaxng /srv/www/obs/docs/api/project.rng $i
+done
+echo
+for i in distributions.xml
+do
+	echo "Validating distributions '$i':"
+	echo -n "- "
+	xmllint --noout --relaxng /srv/www/obs/docs/api/distributions.rng $i
+done
+exit 0
